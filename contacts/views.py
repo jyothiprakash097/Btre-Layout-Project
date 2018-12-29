789559d775487c77c18604a7010583dce577f0b4
jyothiprakash097 <jyothiprakash097@gmail.com>
2018-12-29 19:26:22 +0530

    initial file

---
+++ contacts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from .models import Contact
+def contact(request):
+  if request.method == 'POST':
+    listing_id = request.POST['listing_id']
+    listing = request.POST['listing']
+    name = request.POST['name']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    message = request.POST['message']
+    user_id = request.POST['user_id']
+    realtor_email = request.POST['realtor_email']
+
+
+    # check enquiry already
+
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+        if has_contacted:
+            messages.error(request, 'your request has been submitted,a realtor will be back soon')
+            return redirect('/listings/'+listing_id)
+
+
+    contact = Contact(listing=listing, listing_id=listing_id, name=name,email=email,
+    phone=phone, message=message, user_id=user_id )
+
+    contact.save()
+
+    #send mail
+    send_mail(
+        'Property Listing Inquiry',
+        'There has been a inquiry for ' + listing +'.Sign into the admin panel for more info',
+        'jyothiprakash097@gmail.com',
+        [realtor_email,'jyothiprakash970@gmail.com'],
+        fail_silently=False
+    )
+
+
+
+    messages.success(request, 'your request has been submitted')
+    return redirect('/listings/'+listing_id)
+
+    
