dab987964a4e27bc3af149446cb5a79f30a2b023
jyojthiprakash097 <jyothiprakash097@gmail.com>
2019-02-18 15:08:04 +0530

    second commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn btre.wsgi
+web: python website/manage.py runserver 0.0.0.0:$PORT
--- btre/settings.py
+++ btre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -125,12 +126,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)    
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+
+django_heroku.settings(locals())
 
 
 #media folder
