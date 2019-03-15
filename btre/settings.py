9f23ae7177282f149b634255c83ca88ed7bcfe69
jyojthiprakash097 <jyothiprakash097@gmail.com>
2019-03-15 17:33:06 +0530

    fifth

--- btre/settings.py
+++ btre/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'btredb',
         'USER': 'postgres',
         'PASSWORD': 'jyothi',
